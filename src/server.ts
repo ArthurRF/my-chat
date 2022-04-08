f07e76a420a90121f18846d52d1cb4c97ca474fb
ArthurRFr <arthur_fronza2011@hotmail.com>
2022-04-07 21:10:35 -0300

    FEAT - Add socket and fix public folderrrr

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 
@@ -6,13 +7,19 @@ const app = express();
 
 const server = createServer(app);
 
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('socket: ', socket);
+})
 
 app.get('/', (req, res) => {
   return res.json({
     message: 'Hello chat'
   })
-})
+});
 
 server.listen(3000, () => {
   console.log('Server is running on port 3000.');
